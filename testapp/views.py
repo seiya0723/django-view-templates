f08db21cfb26c76deaa0e48b03ec43036f0da640
Seiya Asahina <seiya@asahina>
2020-11-15 17:08:50 +0900

    django helloworld

---
+++ testapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        return render(request,"index.html")
+
+index   = IndexView.as_view()
